3f3422c7404d6a2c14883d8508d06a04991ce84b
Ryan8765 <rmhaas2211@gmail.com>
2015-01-29 14:04:17 -0600

    Fadein and h2 underlines added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		isWhite : "no",
 		//scroll height function to change color of nav links and background of nav
 		navFunction : function() {
-			//height from top portfolio to top window
+			//height from top portfolio to top document
 			var portfolioDistance = $('#portfolio').offset().top;
 			var scrollPosition = $(window).scrollTop();
 			//height of header
@@ -14,7 +14,6 @@ $(document).ready(function() {
 				$('header').css('background', 'rgba(255,255,255,1)');
 				$('header a').css('color', 'black');
 				this.isWhite = "yes";
-				console.log("first if triggered");
 			} else if (scrollPosition < portfolioDistance - headerHeight && this.isWhite == "yes") {
 				$('header').css('background', 'rgba(255,255,255,.2)');
 				$('header a').css('color', 'white');
@@ -22,20 +21,17 @@ $(document).ready(function() {
 			}
 		}
 	};//end object
-
-	$(window).scroll(function() {
-		navColors.navFunction();
-	});
 	
 	//create a jquery plugin that allows you to scroll to a specific point after clicking a link
 	$.fn.slowScroll = function(clicked) {
 		//grab the links href which is set to the desired element ID you want to scroll to.
+		var headerHeight = $('header').outerHeight();
 		var elementId = clicked.attr('href');
 		$('html, body').animate({
-        	scrollTop: $("#" + elementId).offset().top
+        	scrollTop: ($("#" + elementId).offset().top)-(headerHeight - 1)
     	}, 2000);
     	console.log(elementId);
-	};
+	};//end pluggin function
 
 	$('header a').on('click', function(event){
 		//stop the normal default of clicked "a" link
@@ -44,9 +40,64 @@ $(document).ready(function() {
 		var clicked = $(this);
 		//target your clicked links and pass "clicked" to the jquery pluggin
 		$('header a').slowScroll(clicked);
-	}); 
-	 
+	});
+	//create a function to fadeIn images
+	var fadeElements = {
+		//length of fade
+		fadeTime: 2000,
+		//elements to fade in
+		elementsToFadeIn: $('.triggerFade'),
+		initialLength: $('.triggerFade').length,
+		triggered: "no",
+		elementsToFadeArray: [],
+		alreadyFaded: [],
+		fadeStuff: function() {
+			//updated length of array
+			var length = this.elementsToFadeArray.length;
+			//counter to push into array above
+			var counter = 0;
+			var currentElement;
+			//distance from top of element to top of document
+			var distanceToTop;
+			//scroll position
+			var scrollPosition;
+		
+			//if this is the first time running function change "triggered" to yes and get the array of elements to fade in
+			if(this.triggered == "no") {
+				for (var i = 0; i < this.initialLength; i++) {
+					this.elementsToFadeArray.push(counter);
+					counter++;
+				}
+				this.triggered = "yes";
+			}//end if
+			//if all of the fadein elements aren't showing yet 
+			if (this.alreadyFaded.length < this.initialLength) {
+				//for loop to go through all of elements and see if they are within view
+				for (var i = 0; i < length; i++) {
+					//if you have already shown element continue on 
+					if (this.alreadyFaded.indexOf(i) > -1) continue;
+					console.log("hello");
+					//grab the first element in array
+					currentElement = this.elementsToFadeIn.eq(i);
+					//get distance from top of element to top of document
+					distanceToTop = currentElement.offset().top;
+					//current scroll position
+					scrollPosition = $(window).scrollTop();
+					//if scroll position is greater than distance to top fadein element
+					if (distanceToTop < scrollPosition + window.innerHeight) {
+						currentElement.fadeTo(this.fadeTime, 1);
+						this.alreadyFaded.push(i);
+					}//end if
+				}//end for
+			}//end if
+		}//end fadestuff function
+	};//end fadeData Object
 
+
+	$(window).scroll(function() {
+		navColors.navFunction();
+		fadeElements.fadeStuff();
+	});
 
 	
 //----------------------------------	

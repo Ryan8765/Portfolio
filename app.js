eeb96d9cf8976978b0a1f67394671383b5ebeda4
Ryan8765 <rmhaas2211@gmail.com>
2015-02-17 10:05:04 -0600

    updated animated lines

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 //--------------------------------
-
+	
 	var mobileDevice = "/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/";
 
 	//function to change background of header when screen is resized
@@ -36,6 +36,7 @@ $(document).ready(function() {
 			$('#portfolio').css('margin-top', windowHeight);
 		}
 	};//end portfolioMargin function
+
 	
 
 
@@ -70,28 +71,86 @@ $(document).ready(function() {
 			}
 		}//end navFunction
 	};//end object
+	//correct underlines on smaller devices of they have been triggered by javascript
+	var correctUnderline = function() {
+		if (window.innerWidth < 550) {
+			$('.animateLine').css('padding-left', '32px');
+			$('.animateLine').css('padding-right', '32px');
+		}
+	};
+
+	//animate lines on titles when scrolled into view
+	var animateLines = {
+		animateDuration: 2000,
+		elementsToAnimate: $(".animateLine"),
+		numElements: $('.animateLine').length,
+		elementsAnimated: [],
+		animateTitles: function() {
+			//how far from the top of the webpage is current element
+			var distanceToTop;
+			//where is the scroll position currently
+			var scrollPosition;
+			//current element in question
+			var element;
+			if(this.numElements > this.elementsAnimated.length && window.innerWidth > 550) {
+				//rotate through elements and see if they are within viewing range.
+				for(var i = 0; i < this.numElements; i++) {
+					//current element
+					element = this.elementsToAnimate.eq(i);
+					//if you've already animated this particular element continue on the loop
+					if (this.elementsAnimated.indexOf(i) > -1) continue;
+					//get distance from top of element to top of document
+					distanceToTop = element.offset().top;
+					//current scroll position
+					scrollPosition = $(window).scrollTop();
+					//if scroll position is greater than distance to top animate element
+					if (distanceToTop + 100 < scrollPosition + window.innerHeight && distanceToTop + 400 > scrollPosition + window.innerHeight) {
+						element.animate({
+						    paddingRight: "160px",
+						    paddingLeft: "160px"
+						}, this.animateDuration);
+						//keep track of elements that have already been animated
+						this.elementsAnimated.push(i);
+					}//end if
+				}//end for
+			}//end if
+		}//end animateTitles
+	};
 	
-	//create a jquery plugin that allows you to scroll slowly to a specific point after clicking a link
-	$.fn.slowScroll = function(clicked) {
-		//grab the links href which is set to the desired element ID you want to scroll to.
-		var headerHeight = $('header').outerHeight();
-		var elementId = clicked.attr('href');
-		//if on mobile do scrollTo because animations don't work very well on mobile
-		if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) { 
-			window.scrollTo(0, ($("#" + elementId).offset().top)-(headerHeight - 1));
-		} else {
-			$('html, body').animate({
-	        	scrollTop: ($("#" + elementId).offset().top)-(headerHeight - 1)
-	    	}, 2000);
-	    }
-	};//end pluggin function
+	
+	//create a jquery pluggin that allows you to scroll slowly to a specific point after clicking a link
+	(function ( $ ) {
+		$.fn.slowScroll = function(clicked) {
+			//grab the links href which is set to the desired element ID you want to scroll to.
+			var headerHeightLg = $('.fullSize').outerHeight();
+			//header height for small devices
+			var headerHeightSm = $('.mobileSize').outerHeight();
+			var elementId = clicked.attr('href');
+			//if on mobile do scrollTo because animations don't work very well on mobile
+			if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) { 
+				window.scrollTo(0, ($("#" + elementId).offset().top)-(headerHeight - 1));
+			} else {
+				//if window is greater than 760 use large header for distance
+				if (window.innerWidth > 760) {
+					$('html, body').animate({
+			        	scrollTop: ($("#" + elementId).offset().top)-(headerHeightLg - 1)
+			    	}, 2000);
+			    //else use small header for distance
+			    } else {
+			    	$('html, body').animate({
+			        	scrollTop: $("#" + elementId).offset().top
+			    	}, 2000);
+			    }//end else
+		    }
+		};
+	}( jQuery ));//end pluggin function
 	//scroll to element clicked
 	$('header a').on('click', function(event){
 		//stop the normal default of clicked "a" link
 		event.preventDefault();
 		//grab the specific link clicked on and pass this to the SlowScroll function
 		var clicked = $(this);
-		//target your clicked links and pass "clicked" to the jquery pluggin
+		//target your clicked links and pass "clicked" to the jquery pluggin to scroll to slowly
 		$('header a').slowScroll(clicked);
 	});
 	//create a function to fadeIn images
@@ -151,6 +210,7 @@ $(document).ready(function() {
 		mobileScrollHide();
 		navColors.navFunction();
 		fadeElements.fadeStuff();
+		animateLines.animateTitles();
 	});
 
 	//functions to run on page resize
@@ -158,6 +218,8 @@ $(document).ready(function() {
 		portfolioMargin();
 		headerBackground();
 		navColors.navFunction();
+		fadeElements.fadeStuff();
+		correctUnderline();
 	});
 	
 //----------------------------------	
